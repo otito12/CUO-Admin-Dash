fb77914cc55a8ea3fff9f111065ac3464d976b8c
otito12 <titodarluzu@gmail.com>
2022-11-05 17:21:55 -0400

    netlify redirects

--- backend/utils/routerIndex.js
+++ backend/utils/routerIndex.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // let us know app is working
 // change to spit out list of endpoints
 router.get("/", async (req, res) => {
-  res.json(["App is Working"]);
+  res.json(["App is almost Working"]);
 });
 
 // // connect service routers
